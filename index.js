27535e00030e96e4c088146911ac7b0ae09600dc
yusuf <yjkheraluwala@gmail.com>
2022-06-17 15:13:59 +0530

    added cors

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import routes from './api/routes.js'
 import { init, cleanup } from './whatsapp.js'
 
 const app = express()
+import cors from 'cors'
 const host = process.env.HOST ?? '127.0.0.1'
 const port = parseInt(process.env.PORT ?? 8000)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 app.use('/api', routes)
 
 app.listen(port, host, () => {
